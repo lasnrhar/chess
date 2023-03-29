b6e8a9c3fc62a3a56585713c7283202335c85c69
xiajie <xj@freedomcz.com.cn>
2023-03-29 16:07:06 +0800

    add more content

--- src/app/building.ts
+++ src/app/building.ts
@@ -46,7 +46,7 @@ export class Building {
         100,
         {
           key: 'player',
-          value: 5,
+          value: 10,
         }
       ),
     });
@@ -55,7 +55,7 @@ export class Building {
       key: 'company',
       value: new Building(
         'chess company',
-        'combining your organization into a company makes it more efficient',
+        'combining your group into a company makes it more efficient',
         0,
         500,
         1000,
@@ -70,13 +70,88 @@ export class Building {
       key: 'companyBigger',
       value: new Building(
         'bigger company',
-        "Optimizing the company's structure has increased efficiency.",
+        "optimizing the company's structure has increased efficiency.",
         0,
         5000,
         10000,
         {
           key: 'company',
-          value: 5,
+          value: 3,
+        }
+      ),
+    });
+
+    buildings.push({
+      key: 'companyListed',
+      value: new Building(
+        'listed company',
+        'integrating the company and going public, bring more player to play chess for you.',
+        0,
+        50000,
+        100000,
+        {
+          key: 'companyBigger',
+          value: 3,
+        }
+      ),
+    });
+
+    buildings.push({
+      key: 'country',
+      value: new Building(
+        'small country',
+        'try to purchase a small country, have all the people in the country play chess for you.',
+        0,
+        50000,
+        100000,
+        {
+          key: 'companyListed',
+          value: 3,
+        }
+      ),
+    });
+
+    buildings.push({
+      key: 'countryBigger',
+      value: new Building(
+        'country',
+        'after annexing the surrounding countries, you have become a regional power.',
+        0,
+        500000,
+        1000000,
+        {
+          key: 'country',
+          value: 3,
+        }
+      ),
+    });
+
+    buildings.push({
+      key: 'continent',
+      value: new Building(
+        'continent',
+        'unifying a continent.',
+        0,
+        5000000,
+        10000000,
+        {
+          key: 'countryBigger',
+          value: 3,
+        }
+      ),
+    });
+
+    buildings.push({
+      key: 'earth',
+      value: new Building(
+        'Earth',
+        'now, people all over the world are playing chess for you.',
+        0,
+        5000000,
+        10000000,
+        {
+          key: 'continent',
+          value: 7,
         }
       ),
     });
@@ -86,13 +161,21 @@ export class Building {
   static getBuildCost(key: string, building: Building): number {
     switch (key) {
       case 'player':
-        return building.cost + Math.min(building.cost * 0.01, 1000);
+        return building.cost + Math.min(building.cost * 0.01, 100);
       case 'group':
         return building.cost + Math.min(building.cost * 0.01, 500);
       case 'company':
         return building.cost + Math.min(building.cost * 0.01, 1000);
       case 'companyBigger':
         return building.cost + Math.min(building.cost * 0.01, 5000);
+      case 'companyListed':
+        return building.cost + Math.min(building.cost * 0.01, 10000);
+      case 'country':
+        return building.cost + Math.min(building.cost * 0.01, 50000);
+      case 'countryBigger':
+        return building.cost + Math.min(building.cost * 0.01, 100000);
+      case 'continent':
+        return building.cost + Math.min(building.cost * 0.01, 500000);
       default:
         return building.cost * 1.3;
     }
